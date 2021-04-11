4f1dcb5e451925125d711ab81253ed6e0a88bbc7
Jatin <jatinbabbar1696@gmail.com>
2021-04-11 20:18:58 +0200

    default light theme

--- setup.sh
+++ setup.sh
@@ -6,4 +6,11 @@ port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
+[theme]\n\
+primaryColor="#F63366"\n\
+backgroundColor="#FFFFFF"\n\
+secondaryBackgroundColor="#F0F2F6"\n\
+textColor="#262730"\n\
+font="sans serif"\n\
+\n\
 " > ~/.streamlit/config.toml
