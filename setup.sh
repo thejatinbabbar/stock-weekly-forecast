6b582f876da9fc538d57373a1d23428fdb5c4502
Jatin <jatinbabbar1696@gmail.com>
2021-04-11 20:26:42 +0200

    setup.sh issue fixed

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 mkdir -p ~/.streamlit/
 
-echo "\
+echo '\
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
 [theme]\n\
-primaryColor="#F63366"\n\
-backgroundColor="#FFFFFF"\n\
-secondaryBackgroundColor="#F0F2F6"\n\
-textColor="#262730"\n\
-font="sans serif"\n\
+primaryColor = "#F63366"\n\
+backgroundColor = "#FFFFFF"\n\
+secondaryBackgroundColor = "#F0F2F6"\n\
+textColor = "#262730"\n\
+font = "sans serif"\n\
 \n\
-" > ~/.streamlit/config.toml
+' > ~/.streamlit/config.toml
